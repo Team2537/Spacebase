726bbe03579bb630aeac29ce18d70c98d65b164d
mjorlosky <45836014+mjorlosky@users.noreply.github.com>
2019-01-26 15:00:38 -0500

    Add files via upload

---
+++ ArmSubsystem.java
@@ -0,0 +1,12 @@
+package frc.robot.arm;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class ArmSubsystem extends Subsystem {
+
+    @Override
+    protected void initDefaultCommand() {
+
+    } 
+
+}
---
+++ Robot.java
@@ -0,0 +1,75 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import frc.robot.arm.ArmSubsystem;
+import frc.robot.drive.DriveSubsystem;
+
+public class Robot extends TimedRobot {
+  public static DriveSubsystem driveSys;
+  public static ArmSubsystem armSys;
+  double leftEncoderStartValue, rightEncoderStartValue;
+  private static HumanInput joye;
+  // Use this function for all initialization code
+  @Override
+  public void robotInit() {
+    driveSys = new DriveSubsystem();
+    armSys = new ArmSubsystem();
+
+    registerButtons();
+  }
+
+  public void registerButtons(){
+    HumanInput.registerCommandWhenPressed(HumanInput.increaseHeightCargo, RaiseArmCommand);
+  }
+
+  // Called periodically regardless of the game period
+  @Override
+  public void robotPeriodic() {
+    Scheduler.getInstance().run();
+  }
+
+
+  /* Sandstorm Period */
+  // Called at the beginning of the Sandstorm
+  @Override
+  public void autonomousInit() {
+    leftEncoderStartValue = Robot.driveSys.getEncoderPosLeft();
+    rightEncoderStartValue = Robot.driveSys.getEncoderPosRight();
+  }
+
+  // Called periodically during the Sandstorm
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+
+  /* Teleop Period */
+  // Called at the beginning of the Teleop period
+  @Override
+  public void teleopInit() {
+  }
+  
+  // Called periodically during the Teleop period
+  @Override
+  public void teleopPeriodic() {
+  }
+
+
+
+  @Override
+  public void testInit() {
+    
+  }
+  @Override
+  public void testPeriodic() {
+
+  }
+}
