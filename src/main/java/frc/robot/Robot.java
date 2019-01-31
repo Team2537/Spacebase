452f3272cc42651a02eb3697839bf20dbd6d2962
ewang4255 <ewang3562@gmail.com>
2019-01-31 18:05:40 -0500

    fixed findSecondLargestArea, and wheels now turn to the direction of midpoint

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,23 +4,29 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
+//PRINT EVERYTHING!!!
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import frc.lib.vision.VisionInput;
+import frc.robot.auto.DriveTurnCommand;
 import frc.robot.drive.DriveSubsystem;
+
 
 public class Robot extends TimedRobot {
   public static DriveSubsystem driveSys;
+  public static DriveTurnCommand DriveTurn;
   double leftEncoderStartValue, rightEncoderStartValue;
 
   // Use this function for all initialization code
   @Override
   public void robotInit() {
     driveSys = new DriveSubsystem();
+    driveSys.initDefaultCommand();
     VisionInput.initialize();
+
+    DriveTurn = new DriveTurnCommand();
   }
 
   // Called periodically regardless of the game period
@@ -34,13 +40,18 @@ public class Robot extends TimedRobot {
   // Called at the beginning of the Sandstorm
   @Override
   public void autonomousInit() {
-    leftEncoderStartValue = Robot.driveSys.getEncoderPosLeft();
-    rightEncoderStartValue = Robot.driveSys.getEncoderPosRight();
+    //COMMENTED FOR PEANUT TESTING
+    //leftEncoderStartValue = Robot.driveSys.getEncoderPosLeft();
+    //rightEncoderStartValue = Robot.driveSys.getEncoderPosRight();
+    Scheduler.getInstance().add(new DriveTurnCommand());
+
   }
 
   // Called periodically during the Sandstorm
   @Override
   public void autonomousPeriodic() {
+    Scheduler.getInstance().run();
+     
   }
 
 
--- src/main/java/frc/robot/auto/DriveTurnCommand.java
+++ src/main/java/frc/robot/auto/DriveTurnCommand.java
@@ -6,15 +6,21 @@ import frc.lib.vision.VisionInput;
 import frc.robot.Robot;
 
 public class DriveTurnCommand extends Command{
-//x coord
+
+//MAJOR ISSUES:
+    //CHECK MIDPOINT
+    //WEIRD DRIVETRAIN
+    //x coord
 //thresohld on screen --> line
 //lower or higher than certain pt can adjust wheels
 //640 width, 480 height---300 lower, 340 higher threshold
     public static final int LOWERTHRESHOLD = 300;
     public static final int HIGHERTHRESHOLD = 340;
-    public static final double TURNSPEED = 0.3;
+    public static final double TURNSPEED = 0.4;
     private Point MidPoint;
     private Target[] targets;
+
+
 
     private static double[] getAreas(Target[] targets){
         double[] boundingBoxAreas = new double[targets.length];
@@ -33,39 +39,48 @@ public class DriveTurnCommand extends Command{
         double maxValue = boundingBoxAreas[0]; 
         int maxValueIndex = 0;
  
-        for(int i=0;i < boundingBoxAreas.length;i++){ 
+        for(int i = 0; i < boundingBoxAreas.length; i++){ 
           if(boundingBoxAreas[i] > maxValue){ 
              maxValue = boundingBoxAreas[i]; 
              maxValueIndex = i;
           }
         }
+        System.out.println("MIDPOINT1 from Area: " + maxValueIndex);
         return maxValueIndex;
     }
 
     private static int findSecondLargestAreas(double[] boundingBoxAreas){
-        double smallerMaxValue = boundingBoxAreas[0]; 
+        double first, second;
         int smallerValueIndex = 0;
-
-        for(int i = 0;i < boundingBoxAreas.length;i++){ 
-            if(boundingBoxAreas[i] == findLargestAreas(boundingBoxAreas)){
-                System.out.println("bad");
+        first = second = Double.MIN_VALUE;
+        for ( int i = 0; i < boundingBoxAreas.length; i++) {
+            if (boundingBoxAreas[i] > first) {
+                second = first;
+                first = boundingBoxAreas[i];
             }
-            else if(boundingBoxAreas[i] > smallerMaxValue){ 
-             smallerMaxValue = boundingBoxAreas[i];
-             smallerValueIndex = i; 
-          }
+            // if current element is between first and second, update second to store value of current variable
+            else if (boundingBoxAreas[i] > second && boundingBoxAreas[i] != first){
+                second = boundingBoxAreas[i];
+                smallerValueIndex = i;
+            }
         }
+        System.out.println("MIDPOINT2 from Area2" + second);
         return smallerValueIndex;
     }
+
 
     private static Point getMidpoint(int largeIndex, int LargishIndex, Target[] targets){
         Point box1 = new Point(0, 0);
         box1 = targets[largeIndex].getBoundingBoxCenter();
+        System.out.println("MDPOINT1 from getMidpoint: " + box1);
         Point box2 = new Point(0, 0);
         box2 = targets[LargishIndex].getBoundingBoxCenter();
+        System.out.println("MDPOINT2 from get Midpoint: " + box2);
         Point MidPoint = new Point((box1.x + box2.x)/2, (box1.y + box2.y)/2);
         return MidPoint;
     }
+
+
 
     @Override
     protected void initialize(){
@@ -84,10 +99,33 @@ public class DriveTurnCommand extends Command{
         } else {
             MidPoint = new Point(0, 0);
         }
+        
         System.out.println("MIDPOINT: "+ MidPoint);
+        
         if(MidPoint.x > HIGHERTHRESHOLD || MidPoint.x < LOWERTHRESHOLD){
-            Robot.driveSys.setMotorsLeft(TURNSPEED*Math.signum(320-MidPoint.x));
-            Robot.driveSys.setMotorsRight(-TURNSPEED*Math.signum(320-MidPoint.x));
+           // Robot.driveSys.setMotorsLeft(TURNSPEED*(Math.signum(320-MidPoint.x)));
+
+            if(MidPoint.x > 200 && MidPoint.x < 440) { //when within range of 220 pixels, robot slows down for accuracy
+                
+                if(MidPoint.x < 320) { // if midpoint is too far left
+                    Robot.driveSys.setMotorsLeft(TURNSPEED*((320-MidPoint.x)/120)); //320 - midpoint = distance from middle of screen; 120 is half of full range [200-440)]
+                    Robot.driveSys.setMotorsRight(TURNSPEED*((320-MidPoint.x)/120)); //technically this should be in the negative direction, but the peanut's wired backwards
+                }
+
+                if(MidPoint.x > 320) { // if midpoint is too far right
+                    Robot.driveSys.setMotorsLeft(-TURNSPEED*((MidPoint.x-320)/120));
+                    Robot.driveSys.setMotorsRight(-TURNSPEED*((MidPoint.x-320)/120)); //technically this should be in the positive direction, but the peanut's wired backwards
+                }
+            }
+            else{
+                Robot.driveSys.setMotorsLeft(TURNSPEED);
+                Robot.driveSys.setMotorsRight(TURNSPEED);
+            }
+            //Robot.driveSys.setMotorsLeft(TURNSPEED*z);
+            //midpoint from middle of screen, smaller distance, smaller the speed
+            //Robot.driveSys.setMotorsRight(TURNSPEED*Math.signum(320-MidPoint.x));
+            //Robot.driveSys.setMotorsRight(TURNSPEED*z);
+           
         }
         else{
             Robot.driveSys.setMotors(0, 0);
