33e73002a2f1285221f3d19913adf8b3c3c5de9e
CoolKid235 <lukeedingo@gmail.com>
2019-02-02 11:02:49 -0500

    moved things back inside the project folder

--- src/main/java/frc/robot/HumanInput.java
+++ src/main/java/frc/robot/HumanInput.java
@@ -1,16 +1,32 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.command.Command;
 
 public class HumanInput {
     public static Joystick joystickLeft  = new Joystick(Ports.LEFT_JOYSTICK);
     public static Joystick joystickRight = new Joystick(Ports.RIGHT_JOYSTICK);
 
     // Button aliases
-    // public static Button 
-    
-    public static final int AXIS_X = 0, AXIS_Y = 1;
 
+    //JoystickLeft - Cargo
+    public static Button decreaseheightCargo = new JoystickButton(joystickLeft, 4);
+    public static Button setInitialHeightCargo = new JoystickButton(joystickLeft, 3);
+    public static Button increaseHeightCargo = new JoystickButton(joystickLeft, 5);
+    public static Button intakeCargo = new JoystickButton(joystickLeft, 2);
+    public static Button placeCargo = new JoystickButton(joystickLeft, 1);
+
+    
+    //JoystickRight - Hatch
+    public static Button decreaseheightHatch = new JoystickButton(joystickRight, 4);
+    public static Button setInitialHeightHatch = new JoystickButton(joystickRight, 3);
+    public static Button increaseHeightHatch = new JoystickButton(joystickRight, 5);
+    public static Button intakeHatch = new JoystickButton(joystickRight, 2);
+    public static Button placeHatch = new JoystickButton(joystickRight, 1);
+
+    public static final int AXIS_X = 0, AXIS_Y = 1;
     private static double getJoystickAxis(int axis, Joystick joystick){
         double val = joystick.getRawAxis(axis);
         if(Math.abs(val) <= Specs.JOYSTICK_DEADZONE) return 0;
@@ -21,5 +37,15 @@ public class HumanInput {
     }
     public static double getJoystickAxisRight(int axis){
         return getJoystickAxis(axis, joystickRight);
+    }
+
+    public static void registerCommandWhenPressed(Button button, Command command){
+        button.whenPressed(command);
+    }
+    public static void registerCommandWhileHeld(Button button, Command command){
+        button.whileHeld(command);
+    }
+    public static void registerCommandWhenReleased(Button button, Command command){
+        button.whenReleased(command);
     }
 }
