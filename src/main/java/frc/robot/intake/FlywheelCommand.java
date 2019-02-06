6b8271ff5fc607b31defc64cdb669400f7fca3e1
Alex <rainsiphoner@gmail.com>
2019-02-06 17:12:21 -0500

    infrared sensor added, should be finished for now

--- src/main/java/frc/robot/intake/FlywheelCommand.java
+++ src/main/java/frc/robot/intake/FlywheelCommand.java
@@ -30,7 +30,11 @@ public class FlywheelCommand extends Command {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    if(Robot.intakeSys.infrared.get()){
+      return true;
+    }else{
+      return false;
+    }
   }
 
   // Called once after isFinished returns true
