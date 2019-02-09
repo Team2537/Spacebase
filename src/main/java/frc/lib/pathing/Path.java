28d2be9776b2afe4049f8e2c4c911f38642d5742
reidbhuntley <reidbhuntley@gmail.com>
2019-02-09 12:43:45 -0500

    scaled up the demo path to make it more realistic

--- src/main/java/frc/lib/pathing/Path.java
+++ src/main/java/frc/lib/pathing/Path.java
@@ -184,20 +184,20 @@ public class Path {
         final double dt = 0.001;
         Vec2[] points = new Vec2[]{
             new Vec2(0.0, 0.0),
-            new Vec2(1.0, 1.0),
-            new Vec2(0.5, 2.5),
-            new Vec2(1.5, 3.0),
-            new Vec2(2.0, 2.75),
-            new Vec2(2.0, 1.75),
-            new Vec2(3.0, 1.75),
+            new Vec2(10.0, 10.0),
+            new Vec2(5.0, 25.0),
+            new Vec2(15.0, 30.0),
+            new Vec2(20.0, 27.5),
+            new Vec2(20.0, 17.5),
+            new Vec2(30.0, 17.5),
         };
         
         Waypoint[] waypoints = new Waypoint[points.length];
-        for(int i = 0; i < points.length; i++) waypoints[i] = new Waypoint(points[i], 0);
+        for(int i = 0; i < points.length; i++) waypoints[i] = new Waypoint(points[i], 0);//Double.MAX_VALUE);
 
         Path path = new Path(waypoints);
 
-        RobotConstraints constraints = new RobotConstraints(10, 5, 12);
+        RobotConstraints constraints = new RobotConstraints(30, 15, 12);
         MotionProfile profile = MotionProfileGenerator.generate(constraints, path);
         
         Vec2[] newPoints = new Vec2[(int)(profile.dt()/dt)];
@@ -205,11 +205,12 @@ public class Path {
             newPoints[i] = profile.getState(i*dt).pos;
         }
 
-        Turtle turtle = new Turtle(800,800,180,new Vec2(1,1.5));
+        Turtle turtle = new Turtle(800,800,18,new Vec2(10,15));
         turtle.addPoints(points, Color.red);
         turtle.addPoints(newPoints, Color.blue);
 
         System.out.println(profile.endState().pos);
-        System.out.println(profile.endTime());
+        System.out.println("Time: "+profile.endTime());
+        System.out.println("Error: "+points[points.length-1].diff(profile.endState().pos).mag());
     }
 }
