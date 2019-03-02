ca6cb4e9ce47308e2adbd90f86e001c611b136b6
reidbhuntley <reidbhuntley@gmail.com>
2019-03-02 16:48:37 -0500

    new MotionProfileGenerator

--- src/main/java/frc/lib/pathing/Test.java
+++ src/main/java/frc/lib/pathing/Test.java
@@ -4,13 +4,12 @@ import java.awt.Color;
 
 import frc.lib.motion.DriveSpecs;
 import frc.lib.motion.MotionProfile;
-import frc.lib.pathing.MotionProfileGenerator.ProfileConstraints;
+import frc.lib.motion.Pose2d;
 import frc.lib.util.Turtle;
 import frc.lib.util.Vec2;
 
 public class Test {
     private static Waypoint[] waypoints;
-    private static final double V_kv = 0.05, V_ka = 0.2, V_min = 1;
     private static DriveSpecs drive = new DriveSpecs(12, 6);
     public static void main(String[] args) {
         final double dt = 0.001;
@@ -29,11 +28,7 @@ public class Test {
         waypoints = new Waypoint[points.length];
         for(int i = 0; i < points.length; i++) waypoints[i] = new Waypoint(points[i], 10);
 
-        final double accMax = gss(0, (12-V_min)/V_ka, 1e-5);
-        final double velMax = (12 - V_min - V_ka*accMax)/V_kv;
-        System.out.println("Acc Max: "+accMax+"\nVel Max: "+velMax);
-        ProfileConstraints constraints = new ProfileConstraints(velMax, accMax);
-        MotionProfile profile = MotionProfileGenerator.generate(drive, waypoints, constraints, 0.003);
+        MotionProfile profile = MotionProfileGenerator.generate(drive, new Pose2d(0,0,Math.PI/4), waypoints, 0.003);
         
         Vec2[] newPoints = new Vec2[(int)(profile.dt()/dt)];
         for(int i = 0; i < newPoints.length; i++){
@@ -44,55 +39,10 @@ public class Test {
         turtle.animateProfile(profile);
         turtle.addPoints(points, new Color(255, 140, 0));
         turtle.addPoints(newPoints, Color.blue);
+        turtle.addPoints(MotionProfileGenerator.ptstemp, Color.green);
 
         System.out.println(profile.endState().pose.vec);
         System.out.println("Time: "+profile.endTime());
         System.out.println("Error: "+points[points.length-1].diff(profile.endState().pose.vec).mag());
-    }
-
-    private static double f(double accMax){
-        double velMax = (12 - V_min - V_ka*accMax)/V_kv;
-        ProfileConstraints constraints = new ProfileConstraints(accMax, velMax);
-        MotionProfile profile = MotionProfileGenerator.generate(drive, waypoints, constraints, 0.003);
-        return profile.dt();
-    }
-
-    private static final double invphi = (Math.sqrt(5) - 1) / 2; // 1/phi
-    private static final double invphi2 = (3 - Math.sqrt(5)) / 2; // 1/phi^2
-    private static double gss(double a, double b, double tol){
-        a = Math.min(a,b);
-        b = Math.max(a,b);
-        double h = b - a;
-        if(h <= tol) return (a+b)/2;
-
-        // required steps to achieve tolerance
-        final double n = (int)(Math.ceil(Math.log(tol/h)/Math.log(invphi)));
-        System.out.println(n);
-
-        double c = a + invphi2 * h;
-        double d = a + invphi * h;
-        double yc = f(c);
-        double yd = f(d);
-
-        for(int k = 0; k < n-1; k++){
-            if (yc < yd) {
-                b = d;
-                d = c;
-                yd = yc;
-                h = invphi*h;
-                c = a + invphi2 * h;
-                yc = f(c);
-            } else {
-                a = c;
-                c = d;
-                yc = yd;
-                h = invphi*h;
-                d = a + invphi * h;
-                yd = f(d);
-            }
-        }
-
-        if (yc < yd) return (a+d)/2;
-        else return (c+b)/2;
     }
 }
