1ab24277487c3481ea428b7a68862438968b56b2
Space RAIDer <a@b.c>
2019-03-13 18:02:41 -0400

    testing shuffleboard (broken)

---
+++ shuffleboard2537/src/main/java/frc/TargetsData.java
@@ -0,0 +1,11 @@
+package frc;
+
+import frc.lib.vision.Target;
+
+public class TargetsData {
+    public final Target[] targets;
+
+    public TargetsData(){
+        targets = new Target[0];
+    }
+}
